06876fda9570bfb2b679fc2ee2cbb8fdf88fba1b
Germain OLEA-OYOUGOU <djuhngleen@gmail.com>
2020-03-19 08:29:40 +0100

    modify camera end renderer

--- js/modules/SolarSystem.js
+++ js/modules/SolarSystem.js
@@ -16,17 +16,15 @@ class SolarSystem {
 
         //this.controls = new THREE.TrackballControls(this.camera);
 
-        this.renderer = new THREE.WebGLRenderer ({
-            canvas: this.canvas,
-            alpha: true,
-            antialias: true
-        });
-        //renderer.setSize(width, height);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( this.renderer.domElement );
+
         this.earth = new Earth(0.5, 32, 6, this.scene);
         this.initScene();
 
-        this.camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-        this.camera.position.z = 1.5;
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        this.camera.position.z = 30;
         //webglEl.appendChild(renderer.domElement);
         this.animate();
     }
